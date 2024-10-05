7d9fcb1aca81456959f04c4cc2a36a7dc13d0eab
redoracle <admin@redoracle.com>
2024-10-05 18:24:56 +0100

    Fix variable usage in install_nix.sh for improved extraction process

--- install_nix.sh
+++ install_nix.sh
@@ -71,11 +71,11 @@ echo "Download completed successfully: ${LATEST_VERSION}-x86_64-linux.tar.xz"
 
 # Decompress the tar.xz file
 echo "Decompressing: nix-${LATEST_VERSION}-x86_64-linux.tar.xz"
-xz -d -v "nix-${LATEST_VERSION}-x86_64-linux.tar.xz" || error_exit "Failed to decompress the tar.xz file."
+xz -d -v "${LATEST_VERSION}-x86_64-linux.tar.xz" || error_exit "Failed to decompress the tar.xz file."
 
 # Extract the tar file
-echo "Extracting: nix-${LATEST_VERSION}-x86_64-linux.tar"
-tar xvf "nix-${LATEST_VERSION}-x86_64-linux.tar" || error_exit "Failed to extract the tar file."
+echo "Extracting: ${LATEST_VERSION}-x86_64-linux.tar"
+tar xvf "${LATEST_VERSION}-x86_64-linux.tar" || error_exit "Failed to extract the tar file."
 
 # Rename the extracted directory to 'nix'
 mv "${LATEST_VERSION}-x86_64-linux" nix || error_exit "Failed to rename the extracted directory to 'nix'"
